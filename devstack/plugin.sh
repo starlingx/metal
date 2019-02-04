5133f09a0fb46c2de754e96c4d67cdb0154c966c
Dean Troyer <dtroyer@gmail.com>
2019-02-04 13:49:14 -0600

    Add devstack job and fix linters
    Add the base DevStack job and make sure bashate runs on
    the devstack plugin files.
    
    Begin to re-structure the plugin to match the common structure.
    
    Add devstack/build.sh and split out the build steps into
    separate functions in devstack/lib/stx-metal
    
    This is complete, further work to be done in follow-up changes.
    
    Change-Id: I05f6df758e18f182fb0a05731eddc6cb7f599e51
    Signed-off-by: Dean Troyer <dtroyer@gmail.com>

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -12,21 +12,9 @@ if is_service_enabled stx-metal; then
         echo_summary "Pre-requires of stx-metal"
     elif [[ "$1" == "stack" && "$2" == "install" ]]; then
         # Perform installation of source
-        echo_summary "Install metal"
+        echo_summary "Install stx-metal"
         # maintenance components should be installed in each node
         install_maintenance
-        if is_service_enabled mtce-compute; then
-            echo_summary "Install mtce compute specific services"
-            install_mtce_compute
-        fi
-        if is_service_enabled mtce-control; then
-            echo_summary "Install mtce control specific services"
-            install_mtce_control
-        fi
-        if is_service_enabled mtce-storage; then
-            echo_summary "Install mtce storage specific services"
-            install_mtce_storage
-        fi
     elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
         # Configure after the other layer 1 and 2 services have been configured
         echo_summary "Configure metal"
@@ -34,19 +22,6 @@ if is_service_enabled stx-metal; then
     elif [[ "$1" == "stack" && "$2" == "extra" ]]; then
         # Initialize and start the metal services
         echo_summary "Initialize and start metal "
-        if is_service_enabled mtce-control; then
-            echo_summary "Start mtce control specific services"
-            # Start Agents on controller node
-            start_mtce_control
-        fi
-        if is_service_enabled mtce-compute; then
-            echo_summary "Start mtce compute specific services"
-            start_mtce_compute
-        fi
-        if is_service_enabled mtce-storage; then
-            echo_summary "Start mtce storage specific services"
-            start_mtce_storage
-        fi
         # Start services on each node
         start_maintenance
     elif [[ "$1" == "stack" && "$2" == "test" ]]; then
@@ -59,19 +34,6 @@ if is_service_enabled stx-metal; then
         echo_summary "Stop metal services"
         # Stop client services on each node
         stop_maintenance
-        if is_service_enabled mtce-control; then
-            echo_summary "Stop mtce control specific services"
-            # Stop Agents on controller node
-            stop_mtce_control
-        fi
-        if is_service_enabled mtce-compute; then
-            echo_summary "Stop mtce compute specific services"
-            stop_mtce_compute
-        fi
-        if is_service_enabled mtce-storage; then
-            echo_summary "Stop mtce storage specific services"
-            stop_mtce_storage
-        fi
     fi
 
     if [[ "$1" == "clean" ]]; then
