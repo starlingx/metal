b5d22ef3e7dbefdd2c6337b00eb9788abfae0e0f
Eric MacDonald <eric.macdonald@windriver.com>
2022-11-15 23:49:29 +0000

    Create new pxeboot feed refresh script and service
    This update introduces a new script that can be called
    by patching to refresh the kernel, initrd and other
    system node install feed staged files in support of
    kernel patching.
    
    This update also introduces and enables new service file
    that triggers the creation of the pxeboot feeds or refreshes
    the pxeboot feeds if what they contain does not match the
    content in /boot.
    
    Both new script and service files are added to the
    pxe-network-installer package so they get installed
    into the filesystem properly.
    
    Lastly, there are 2 kickstart changes implemented.
     1. The kickstart code that copied the kickstart files from
          /var/www/pages/feed/rel-xx.xx/
          to
          /var/www/pages/feed/rel-xx.xx/kickstart
        is removed in favor of the pxe-network-installer package
        doing that automatically.
     2. The kickstart is modified to remove the previous pxeboot
        feed fetch and creation function.
        One exception to this is the efi.img file, its fetch remains.
        Note the efi image is currenly not included in the /boot dir.
    
    Test Plan:
    
    PASS: Verify Debian build and AIO DX install (cd and pxe installs)
    PASS: Verify Debian Standard 2+1 DX system install
    PASS: In above cases verify end-to-end handling of the following
          test case staging.
    PASS: Verify pxeboot feed staging on subcloud controller-0 install
    PASS: Verify pxeboot feed file positioning in
          - /var/pxeboot/rel-xx.xx (kernel and initrd images)
          - /var/www/pages/feed/rel-xx.xx/pxeboot (kernel/initrd images)
          - /var/www/pages/feed/rel-xx.xx/pxeboot/EFI/BOOT (other files)
          - /var/pxeboot and /var/www/pages/feed/rel-xx.xx (efi.img)
    PASS: Verify rsync bypass for the above cases when the files match
          - complete and partial cases
    PASS: Verify staging when the stage dirs are missing
          - complete and partial cases
    PASS: Verify staging when stage files mismatch
          - complete and partial cases
    PASS: Verify service enable on controllers for AIO and STD configs
    PASS: Verify kickstart file position change
    PASS: Verify shellcheck static analysis
    PASS: Verify pxeboot_feed.sh script error handling
    PASS: Verify pxeboot_feed.sh script logging
    
    Story: 2009968
    Task: 46789
    Signed-off-by: Eric MacDonald <eric.macdonald@windriver.com>
    Change-Id: Ic98b2686c417103749cb777adb28ac73ac1d397c

---
+++ installer/pxe-network-installer/pxe-network-installer/pxeboot_feed.sh
@@ -0,0 +1,219 @@
+#!/bin/bash
+#############################################################################
+# Copyright (c) 2022 Wind River Systems, Inc.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# This utility is used to create or update the following directories
+#
+#    /var/www/pages/feed/rel-xx.xx/pxeboot
+#    /var/pxeboot/rel-xx.xx
+#
+# ... with the kernel, initrd and other images and signature files from /boot
+#
+#############################################################################
+#
+# chkconfig: 2345 98 2
+#
+### BEGIN INIT INFO
+# Provides: pxeboot_feed
+# Required-Start: $null
+# Required-Stop: $null
+# Default-Start: 3 5
+# Default-Stop: 0 1 2 6
+# Short-Description: StarlingX Installer Pxeboot Feed Refresh
+### END INIT INFO
+
+# Script has options 'debug' argument
+debug=false
+[ -n "${1}" ] && [ "${1}" == "debug" ] && debug=true
+
+LOG_TAG=${LOG_TAG:-$(basename "${0}")}
+
+# return code
+RETVAL=1
+
+#############################################################################
+# Name     : ilog
+# Purpose  : log info message
+# Parmaeter: message to log
+# Returns  : none
+#############################################################################
+
+function ilog {
+    logger -t "${LOG_TAG}" "${@}"
+}
+
+#############################################################################
+# Name     : dlog
+# Purpose  : log and echo debug messages
+# Parmaeter: message to log
+# Returns  : none
+#############################################################################
+
+function dlog {
+    if [ "${debug}" == true ] ; then
+        logger -t "${LOG_TAG}" "${@}"
+    fi
+}
+
+#############################################################################
+# Name       : rsync_if_not_equal
+# Purpose    : Speed up the refresh service.
+# Assumptions: Reads are faster thyan writes
+#              Equal case is more likely
+# Parameters : $1 - src path/file
+#              $2 - dst path/file
+# Returns    : none
+#############################################################################
+
+function rsync_if_not_equal {
+    local src_file="${1}"
+    local dst_file="${2}"
+    local need_rsync=false
+
+    if [ -e "${src_file}" ] ; then
+        if [ -e "${dst_file}" ] ; then
+            src=( $(md5sum "${src_file}") )
+            dst=( $(md5sum "${dst_file}") )
+            if [ "${src[0]}" == "${dst[0]}" ] ; then
+                dlog "bypass rsync ; ${src_file}" and "${dst_file} are equal"
+            else
+                need_rsync=true
+            fi
+        else
+            need_rsync=true
+        fi
+    else
+        ilog "Warning: '${src_file}' not found"
+    fi
+
+    if [ "${need_rsync}" = true ] ; then
+        ilog "syncing ${src_file} to ${dst_file}"
+        rsync "${src_file}" "${dst_file}"
+    fi
+}
+
+#############################################################################
+
+# Override release with what is found in platform.conf
+rel=""
+if [ -e "/etc/platform/platform.conf" ] ; then
+    rel=$(grep sw_version < /etc/platform/platform.conf | cut -d '=' -f 2)
+fi
+if [ -z "${rel}" ] ; then
+    rel="xxxSW_VERSIONxxx"
+fi
+
+# pxeboot objects path
+feed="/var/www/pages/feed/rel-${rel}"
+pxefeed="${feed}/pxeboot"
+pxeboot="/var/pxeboot"
+
+# ensure the deepest directories are created
+if [ ! -d "${pxefeed}/EFI/BOOT" ] ; then
+    mkdir -p "${pxefeed}/EFI/BOOT" > /dev/null 2>&1 || exit ${RETVAL}
+fi
+if [ ! -d "${pxeboot}rel-${rel}" ] ; then
+    mkdir -p "${pxeboot}/rel-${rel}" > /dev/null 2>&1 || exit ${RETVAL}
+fi
+if [ ! -d "${pxeboot}/EFI/BOOT" ]  ; then
+    mkdir -p "${pxeboot}/EFI/BOOT" > /dev/null 2>&1 || exit ${RETVAL}
+fi
+
+base_path="/boot/ostree"
+declare -a file_list=()
+
+if [ ! -d "${base_path}" ] ; then
+    ilog "Error: base path '${base_path}' does not exist"
+    exit ${RETVAL}
+fi
+
+file_list=( $(find "${base_path}" -name 'initramfs*') )
+file_list+=( $(find "${base_path}" -name 'vmlinuz*') )
+dlog "${file_list[*]}"
+for f in "${file_list[@]}" ; do
+    path_file1=""
+    filename=$(basename "${f}")
+    dlog "File: ${filename} ... ${f}"
+    if [ "${filename}" == "initramfs.sig" ] ; then
+        path_file1="${pxeboot}/rel-${rel}/initrd.sig"
+        path_file2="${pxefeed}/initrd.sig"
+    elif [ "${filename}" == "initramfs" ] ; then
+        path_file1="${pxeboot}/rel-${rel}/initrd"
+        path_file2="${pxefeed}/initrd"
+    elif [ "${filename}" == "vmlinuz.sig" ] ; then
+        path_file1="${pxeboot}/rel-${rel}/bzImage.sig"
+        path_file2="${pxefeed}/bzImage.sig"
+    elif [ "${filename}" == "vmlinuz" ] ; then
+        path_file1="${pxeboot}/rel-${rel}/bzImage"
+        path_file2="${pxefeed}/bzImage"
+    elif [[ "${filename}" == *"rt-amd64.sig"* ]] ; then
+        path_file1="${pxeboot}/rel-${rel}/bzImage-rt.sig"
+        path_file2="${pxefeed}/bzImage-rt.sig"
+    elif [[ "${filename}" == *"rt-amd64"* ]] ; then
+        path_file1="${pxeboot}/rel-${rel}/bzImage-rt"
+        path_file2="${pxefeed}/bzImage-rt"
+    elif [[ "${filename}" == *"amd64.sig"* ]] ; then
+        path_file1="${pxeboot}/rel-${rel}/bzImage-std.sig"
+        path_file2="${pxefeed}/bzImage-std.sig"
+    elif [[ "${filename}" == *"amd64"* ]] ; then
+        path_file1="${pxeboot}/rel-${rel}/bzImage-std"
+        path_file2="${pxefeed}/bzImage-std"
+    else
+        ilog "ignoring unknown file: ${f}"
+        continue
+    fi
+
+    rsync_if_not_equal "${f}" "${path_file1}"
+    rsync_if_not_equal "${f}" "${path_file2}"
+done
+
+
+# Other image files
+file1="LockDown.efi.sig"
+file2="LockDown.efi"
+file3="bootx64.efi"
+file4="grub.cfg.sig"
+file5="grubx64.efi"
+file6="mmx64.efi"
+
+file_list=( $(find "${base_path}" \
+    -name "${file1}" -o \
+    -name "${file2}" -o \
+    -name "${file3}" -o \
+    -name "${file4}" -o \
+    -name "${file5}" -o \
+    -name "${file6}") )
+
+dlog "${file_list[*]}"
+
+for f in "${file_list[@]}" ; do
+    filename=$(basename "${f}")
+    dlog "File: ${filename} ... ${f}"
+
+    path_file="EFI/BOOT/${filename}"
+    path_file1=""
+
+    if [[ "${filename}" == *"${file1}"* || \
+        "${filename}" == *"${file2}"* || \
+        "${filename}" == *"${file3}"* || \
+        "${filename}" == *"${file4}"* || \
+        "${filename}" == *"${file5}"* || \
+        "${filename}" == *"${file6}"* ]] ; then
+        path_file1="${pxeboot}/${path_file}"
+        path_file2="${pxefeed}/${path_file}"
+    else
+        ilog "ignoring unknown file: ${f}"
+        continue
+    fi
+
+    rsync_if_not_equal "${f}" "${path_file1}"
+    rsync_if_not_equal "${f}" "${path_file2}"
+done
+
+# rsync efi.img file
+rsync_if_not_equal "${pxeboot}/efi.img" "${feed}/efi.img"
+
+RETVAL=0
+exit ${RETVAL}
