d9d7111cd8f818433e7adba4d90faab82eb8aff1
junfeng-li <junfeng.li@windriver.com>
2023-04-28 15:21:36 +0000

    Refactor from-load pxe setup in to-load kickstart
    Details: Since this is the first Debian to Debian upgrade,
    we need to refactor some existing code that is used for
    centOS upgrade.
    
    This commit is
    
    	1. clean up CentOS related code
    	2. copy to-load pxeboot-update.sh to /etc/ during the import
    	   instead of /usr/sbin/
    	3. install to-load pxeboot-update.sh in /etc/ instead of
    	   /usr/sbin/ during iso installation
    	4. update kickstart Upgrade Support accordingly to set up
        	   from-load feed
    
    Test Plan:
    
    PASS: built the iso for upgrade
    PASS: upgraded from 22.12 to 23.09 in DX
    PASS: downgrade from 23.09 to 22.12 in DX
    
    Depends-on: https://review.opendev.org/c/starlingx/tools/+/881882
    
    Task: 47805
    Story: 2010651
    Signed-off-by: Junfeng (Shawn) Li <junfeng.li@windriver.com>
    Change-Id: Ia67f5f3e66f5b33c3d7fb8d93a15547d59eaa71e

--- bsp-files/upgrades/import.sh
+++ bsp-files/upgrades/import.sh
@@ -50,75 +50,37 @@ fi
 rm -rf ${FEED_DIR}
 mkdir -p ${FEED_DIR}
 
-# Centos will be temporarily supported for testing. The import code
-# for Centos 22.12 will be removed soon. No need for any fancy way
-# to detect if it is a Debian iso, just check existance of ostree_repo
-# TODO: remove the "else" clause
-if [ -d ${ISO_DIR}/ostree_repo ]; then
-    # it is a Debian iso.
+# copy pxeboot, kickstart, ostree_repo to feed directory
+echo "Copy kickstart to ${FEED_DIR}"
+cp -rp ${ISO_DIR}/kickstart ${FEED_DIR}/
+echo "Copy pxeboot to ${FEED_DIR}"
+cp -rp ${ISO_DIR}/pxeboot ${FEED_DIR}/
+echo "Copy ostree_repo to ${FEED_DIR}"
+cp -rp ${ISO_DIR}/ostree_repo ${FEED_DIR}/
 
-    # copy pxeboot, kickstart, ostree_repo to feed directory
-    echo "Copy kickstart to ${FEED_DIR}"
-    cp -rp ${ISO_DIR}/kickstart ${FEED_DIR}/
-    echo "Copy pxeboot to ${FEED_DIR}"
-    cp -rp ${ISO_DIR}/pxeboot ${FEED_DIR}/
-    echo "Copy ostree_repo to ${FEED_DIR}"
-    cp -rp ${ISO_DIR}/ostree_repo ${FEED_DIR}/
+echo "Copy install_uuid to ${FEED_DIR}"
+cp ${CURRENT_FEED_DIR}/install_uuid ${FEED_DIR}/
 
-    echo "Copy install_uuid to ${FEED_DIR}"
-    cp ${CURRENT_FEED_DIR}/install_uuid ${FEED_DIR}/
+mkdir ${FEED_DIR}/upgrades
+echo "Copy pxeboot-update-${VERSION}.sh to ${FEED_DIR}/upgrades"
+cp ${ISO_DIR}/upgrades/pxeboot-update-${VERSION}.sh ${FEED_DIR}/upgrades/
 
-    mkdir ${FEED_DIR}/upgrades
-    echo "Copy pxeboot-update-${VERSION}.sh to ${FEED_DIR}/upgrades"
-    cp ${ISO_DIR}/upgrades/pxeboot-update-${VERSION}.sh ${FEED_DIR}/upgrades/
+echo "Copy efi.img to ${FEED_DIR}"
+cp ${ISO_DIR}/efi.img ${FEED_DIR}/
+# for upgrade from 22.06 to Debian 22.12, patch during upgrade is not supported
 
-    echo "Copy efi.img to ${FEED_DIR}"
-    cp ${ISO_DIR}/efi.img ${FEED_DIR}/
-    # for upgrade from 22.06 to Debian 22.12, patch during upgrade is not supported
-    # only copy the patch meta is enough
-    if [ -d ${ISO_DIR}/patches ]; then
-        rsync -ac ${ISO_DIR}/patches/ /opt/patching/metadata/committed/
-        # copy patch metadata to feed, so to be picked up by kickstart to copy to release N+1
-        mkdir ${FEED_DIR}/patches -p
-        cp -a ${ISO_DIR}/patches/* ${FEED_DIR}/patches/
-    fi
-else
-    # this is a Centos iso
-    trap cleanup 0
+echo "Copy pxeboot-update-${SW_VERSION}.sh to ${CURRENT_FEED_DIR}/upgrades"
+mkdir -p ${CURRENT_FEED_DIR}/upgrades/
+# In stx 8.0, the pxeboot-update-22.12.sh is in /usr/sbin
+# In stx 9.0, the pxeboot-update-23.09.sh is in /etc due to the ostree managed
+# /usr directory.
+cp  -rp /usr/sbin/pxeboot-update-22.12.sh ${CURRENT_FEED_DIR}/upgrades/ 2>/dev/null || \
+cp  -rp /etc/pxeboot-update-${SW_VERSION}.sh ${CURRENT_FEED_DIR}/upgrades/
 
-    TMP_RPM=/tmp/cpio
-
-    rm -rf ${TMP_RPM}
-
-    cp -rp ${ISO_DIR}/Packages ${ISO_DIR}/repodata ${ISO_DIR}/LiveOS ${FEED_DIR}/
-
-    cp -p ${CURRENT_FEED_DIR}/install_uuid ${FEED_DIR}/
-
-    if [ -d ${ISO_DIR}/patches ]; then
-        mkdir -p /var/www/pages/updates/rel-${VERSION}
-        cp -r ${ISO_DIR}/patches/Packages ${ISO_DIR}/patches/repodata /var/www/pages/updates/rel-${VERSION}/
-        rsync -ac ${ISO_DIR}/patches/metadata/ /opt/patching/metadata/
-        mkdir -p /opt/patching/packages/${VERSION}
-
-        find /var/www/pages/updates/rel-${VERSION}/Packages -name '*.rpm' \
-            | xargs --no-run-if-empty -I files cp --preserve=all files /opt/patching/packages/${VERSION}/
-    fi
-
-    # copy package checksum if it exists
-
-    PKG_FILE="package_checksums"
-    PKG_FILE_LOC=/usr/local/share/pkg-list
-
-    if [ -f ${ISO_DIR}/${PKG_FILE} ]; then
-
-        DEST_PKG_FILE="${VERSION}_packages_list.txt"
-        if [ ! -d ${PKG_FILE_LOC} ]; then
-            mkdir -p ${PKG_FILE_LOC}
-        fi
-
-        cp ${ISO_DIR}/${PKG_FILE} ${PKG_FILE_LOC}/${DEST_PKG_FILE}
-        cp ${ISO_DIR}/${PKG_FILE} ${FEED_DIR}/${PKG_FILE}
-    fi
-fi
+# The pxelinux.cfg.files directory is from the current release feed in Debian.
+echo "Copy pxelinux.cfg.files directory to ${CURRENT_FEED_DIR}"
+mkdir -p ${CURRENT_FEED_DIR}/pxeboot/pxelinux.cfg.files/
+find /var/pxeboot/pxelinux.cfg.files -type f ! -name "*${VERSION}" \
+-exec cp -p {} ${CURRENT_FEED_DIR}/pxeboot/pxelinux.cfg.files/ \;
 
 echo 'import has completed'
